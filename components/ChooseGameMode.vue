51f942ffdc3921ea2c672ec4d0919e8b7db68a9e
Tamara <tamara.andrejevic@gmail.com>
2022-12-09 12:13:31 +0100

    test files

---
+++ components/ChooseGameMode.vue
@@ -0,0 +1,36 @@
+<template>
+<div>
+  <header id="header" style="justify-content:center;">
+    <div class="title">
+      ROCK, PAPER, SCISSORS
+    </div>
+  </header>
+  <div class="chooseGameText">CHOOSE GAME MODE</div>
+  <div class="chooseModeDiv">
+    <router-link to="/playermode">
+      <button class="basicBtn modeBtn" @click="goToMode('player')">Player Mode</button>
+    </router-link>
+    <router-link to="/computermode">
+      <button class="basicBtn modeBtn" @click="goToMode('computer')">Computer Mode</button>
+    </router-link>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: "ChooseGameMode",
+methods: {
+  goToMode(mode) {
+    if(mode=='computer'){
+      this.$store.state.computerMode=true;
+    } else {
+      this.$store.state.computerMode=false;
+    }
+    this.$store.state.counter=0;
+    this.$store.state.counterComputer=0;
+    this.$store.state.computerModeCounter=0;
+  }
+}
+}
+</script>
---
+++ components/HeaderComp.vue
@@ -0,0 +1,23 @@
+<template>
+  <header id="header">
+    <div class="title">
+      ROCK, PAPER, SCISSORS
+    </div>
+    <div class="scoreDiv">
+    <div><img width="50px" src="../assets/images/flag.png" /></div>
+    <div id="scoreBox">
+      <span class="scoreResult">Score</span>
+      <span v-if="!$store.state.computerMode" id="score" class="playerScore">{{
+        $store.state.counter +':'+ $store.state.counterComputer}}</span>
+      <span v-else id="score" class="computerScore">{{
+        $store.state.computerModeCounter +':'+ $store.state.counterComputer}}</span>
+    </div>
+  </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "HeaderComp",
+}
+</script>
